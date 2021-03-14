9c258fbf86c07a071b9f8d6bba0bff2df9ba44f5
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-14 00:44:41 -0700

    linter is yelling at me

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="container">
-  <div class="row">
-    <!-- NOTE v-for is grabbing a singular post from state -->
-    <!-- NOTE Key identifies child post from component -->
-    <!-- NOTE postData is a prop grabbing the object type with its name -->
-    <Post v-for="postData in state.posts" :key="postData.id" :post="postData" />
+  <div class="container">
+    <div class="row">
+      <!-- NOTE v-for is grabbing a singular post from state -->
+      <!-- NOTE Key identifies child post from component -->
+      <!-- NOTE postData is a prop grabbing the object type with its name -->
+      <Post v-for="postData in state.posts" :key="postData.id" :post="postData" />
+    </div>
   </div>
-</div>
 </template>
 
 <script>
